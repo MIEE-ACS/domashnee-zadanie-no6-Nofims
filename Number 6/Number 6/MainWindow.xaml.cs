d925c28e4654594d2a91c1674be17a2fd147fdfd
Nofims <eld170916@icloud.com>
2021-01-28 19:22:19 +0300

    Update MainWindow.xaml.cs

--- Number 6/Number 6/MainWindow.xaml.cs
+++ Number 6/Number 6/MainWindow.xaml.cs
@@ -27,25 +27,41 @@ namespace Number_6
         //public virtual string food { get; set; }
        // public virtual string habitat { get; set; }
         virtual public int age { get; set; }
-
-        //virtual public int Counter
+        public int counter = 1;
+        virtual public int Counter
         {
-          //  get
-            //{
-              //  return counter;
-            //}
-        //}
+            get
+            {
+                return counter;
+            }
+        }
 
+                
         class mammal : pets
         {
-            public string live
-            {
-                get
+            
+            
+           
+            class cat : mammal
+            { 
+
+                double catage;
+                public string live
                 {
-                    return "Flat";
+                    get
+                    {
+                        return "Квартира";
+                    }
                 }
-            }
-            public double agee
+                public string quirk
+                {
+                    get
+                    {
+                        return "Каждый день приносит мышку";
+                    }
+                }
+
+                public override int age
             {
                 get
                 {
@@ -64,15 +80,13 @@ namespace Number_6
                     }
                     else
                     {
-                        agee = value;
+                        catage = value;
                     }
                 }
             }
-            // string ;  string ; double ; 
-            class cat : mammal
-            {
-                double age;
-                    public string food
+
+
+                public string food
 
                 {
                     get
@@ -92,47 +106,463 @@ namespace Number_6
 
                 }
             }
-            class ferret : mammal
+        class dog : mammal
         {
+          public double  dogage;
+            public string live
+            {
+                get
+                {
+                    return "Квартира";
+                }
+            }
+            public string quirk
+            {
+                get
+                {
+                    return "Нравится , когда глядят живот";
+                }
+            }
+            public string food
+
+            {
+                get
+                {
+                    return "Chappi";
+                }
+            }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    dogage = value;
+                }
+            }
+
+        }
             public string type
             {
                 get
                 {
-                    return "Хорёк";
+                    return "Собака";
                 }
 
             }
-            public string food
+
+        }
+    
+
+    
+        }
+        class reptile : pets
+{
+   
+    class chameleon : reptile
+    {
+        public double chamage;
+            public string live
             {
                 get
                 {
-                    return "Спецкорм";
+                    return "Вольер";
                 }
             }
-        }
-        }
-        class reptile : pets
+            public string food
+
         {
-            class chameleon : reptile { }
-            class iguana : reptile { }
+            get
+            {
+                return "Насекомые";
+            }
         }
-        class bird : pets
-        {
-            class parrot : bird { }
-            class canary : bird { }
+            public string quirk
+            {
+                get
+                {
+                    return "Не любит менять цвет";
+                }
+            }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    chamage = value;
+                }
+            }
 
         }
+        public string type
+        {
+            get
+            {
+                return "Хамелеон";
+            }
+
+        }
+
+    }
+
+    class iguana : reptile
+    {
+        public double iguage;
+            public string live
+            {
+                get
+                {
+                    return "Вольер";
+                }
+            }
+            public string food
+
+        {
+            get
+            {
+                return "Фрукты";
+            }
+        }
+            public string quirk
+            {
+                get
+                {
+                    return "Шипит при виде кошки";
+                }
+            }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    iguage = value;
+                }
+            }
+
+        }
+        public string type
+        {
+            get
+            {
+                return "Игуана";
+            }
+
+        }
+
+    }
+}
+        class bird : pets
+        {
+   
+    class parrot : bird
+    {
+        public double parrage;
+            public string live
+            {
+                get
+                {
+                    return "Клетка";
+                }
+            }
+            public string quirk
+            {
+                get
+                {
+                    return "Гадит на улице";
+                }
+            }
+            public string food
+
+        {
+            get
+            {
+                return "Семечки";
+            }
+        }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    parrage = value;
+                }
+            }
+
+        }
+        public string type
+        {
+            get
+            {
+                return "Попугай";
+            }
+
+        }
+
+    }
+    class canary : bird
+    {
+        public double canage;
+            public string live
+            {
+                get
+                {
+                    return "Клетка";
+                }
+            }
+            public string food
+
+        {
+            get
+            {
+                return "Кукуруза сушеная";
+            }
+        }
+            public string quirk
+            {
+                get
+                {
+                    return "Любит еду желтого цвета";
+                }
+            }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                   canage = value;
+                }
+            }
+
+        }
+        public string type
+        {
+            get
+            {
+                return "Попугай";
+            }
+
+        }
+
+    }
+
+}
         class fish : pets
         {
-            class carp : fish { }
-            class goldfish : fish { }
-        }
-        public partial class MainWindow : Window
+    class carp : fish
+    {
+        public double carpage;
+            public string live
+            {
+                get
+                {
+                    return "Пруд";
+                }
+            }
+
+            public string quirk
+            {
+                get
+                {
+                    return "Плавает с друзьями";
+                }
+            }
+            public string food
+
         {
-            public MainWindow()
+            get
+            {
+                return "Корм для рыб";
+            }
+        }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    carpage = value;
+                }
+            }
+
+        }
+        public string type
+        {
+            get
+            {
+                return "Карп";
+            }
+
+        }
+
+    }
+    class goldfish : fish
+    {
+        public double goldage;
+            public string live
+            {
+                get
+                {
+                    return "Аквариум";
+                }
+            }
+            public string quirk
+            {
+                get
+                {
+                    return "Исполняет желания только 13-го в пятницу";
+                }
+            }
+            public string food
+
+        {
+            get
+            {
+                return "Корм для рыб 'Особый' ";
+            }
+        }
+            public override int age
+            {
+            get
+            {
+                return age;
+
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Возраст не может быть отрицательным!");
+                }
+                else if (value == 0)
+                {
+                    throw new Exception("Возраст не может быть равным нулю , так как не только что родился!");
+                }
+                else
+                {
+                    goldage = value;
+                }
+            }
+
+        }
+        public string type
+        {
+            get
+            {
+                return "Золотая рыбка";
+            }
+
+        }
+
+    }
+}
+        public partial class MainWindow : Window
+    {
+        
+        
+
+        List<pets> pet = new List<pets>()
+        {
+             new fish() {age = 3 },
+            new fish() { age = 2},
+             new fish() { age = 3},
+
+            new reptile()  { age = 15},
+            new reptile()  { age = 7},
+            new reptile()  { age = 2},
+
+            new bird() { age = 6},
+            new bird() { age = 4},
+          new bird() { age = 20},
+
+          
+        };
+
+        public MainWindow()
             {
                 InitializeComponent();
             }
         }
     }
-}
+
